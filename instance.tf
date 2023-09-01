1a9d8accf5a51fc4f83c8844fdd4ed3f8cfc32c2
Dylan Tey <dylantey@users.noreply.github.com>
2023-08-31 22:32:20 -0400

    New instance (#5)
    * Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    civo = {
+      source = "civo/civo"
+    }
+  }
+}
+
+# Configure the Civo Provider
+provider "civo" {
+  token = var.civo_token
+}
+
+# Query instance disk image
+data "civo_disk_image" "debian" {
+   filter {
+        key = "name"
+        values = ["debian-10"]
+   }
+}
+
+# Create a new instance
+resource "civo_instance" "test" {
+    tags = ["python", "nginx"]
+    notes = "this is a note for the server"
+    size = "g3.small"
+    disk_image = element(data.civo_disk_image.debian.diskimages, 0).id
+}
