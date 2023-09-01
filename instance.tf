c4444b9e22b3e50dd76c075c1d758c84efb0517e
Dylan Tey <dylantey@users.noreply.github.com>
2023-08-31 22:28:58 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -11,21 +11,6 @@ provider "civo" {
   token = var.civo_token
 }
 
-# Query small instance size
-data "civo_instances_size" "small" {
-    filter {
-        key = "name"
-        values = ["g3.small"]
-        match_by = "re"
-    }
-
-    filter {
-        key = "type"
-        values = ["instance"]
-    }
-
-}
-
 # Query instance disk image
 data "civo_disk_image" "debian" {
    filter {
@@ -38,6 +23,6 @@ data "civo_disk_image" "debian" {
 resource "civo_instance" "test" {
     tags = ["python", "nginx"]
     notes = "this is a note for the server"
-    size = element(data.civo_instances_size.small.sizes, 0).name
+    size = "g3.small"
     disk_image = element(data.civo_disk_image.debian.diskimages, 0).id
 }
