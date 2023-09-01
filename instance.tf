58ec765f71f4600e3e5134ade634d12680f5d511
Dylan Tey <dylantey@users.noreply.github.com>
2023-08-31 22:24:07 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,7 +1,30 @@
-resource "civo_instance" "github-action" {
-    hostname = "github-action.com"
-    tags = ["test", "github-action"]
+# Query small instance size
+data "civo_instances_size" "small" {
+    filter {
+        key = "name"
+        values = ["g3.small"]
+        match_by = "re"
+    }
+
+    filter {
+        key = "type"
+        values = ["instance"]
+    }
+
+}
+
+# Query instance disk image
+data "civo_disk_image" "debian" {
+   filter {
+        key = "name"
+        values = ["debian-10"]
+   }
+}
+
+# Create a new instance
+resource "civo_instance" "test" {
+    tags = ["python", "nginx"]
     notes = "this is a note for the server"
-    initial_user = "root"
-    size = "g2.xsmall"
+    size = element(data.civo_instances_size.small.sizes, 0).name
+    disk_image = element(data.civo_disk_image.debian.diskimages, 0).id
 }
