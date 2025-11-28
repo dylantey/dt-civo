06deea23e68da067f1934c9f0deaa3b1084db57e
Dylan Tey <dylantey@Dylans-Laptop.local>
2025-11-28 15:53:13 -0500

    feat: replace instance with k3s cluster

--- instance.tf
+++ instance.tf
@@ -8,21 +8,13 @@ terraform {
 
 # Configure the Civo Provider
 provider "civo" {
+  region = "NYC1"
   token = var.civo_token
 }
 
-# Query instance disk image
-data "civo_disk_image" "debian" {
-   filter {
-        key = "name"
-        values = ["debian-10"]
-   }
-}
-
-# Create a new instance
-resource "civo_instance" "test" {
-    tags = ["python", "nginx"]
-    notes = "this is a note for the server"
-    size = "g3.medium"
-    disk_image = element(data.civo_disk_image.debian.diskimages, 0).id
+# Create a cluster
+resource "civo_kubernetes_cluster" "dt-cluster" {
+    name = "dt-cluster"
+    num_target_nodes = 1
+    target_nodes_size = "g4s.kube.xsmall"
 }
